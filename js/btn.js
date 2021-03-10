fa2b539a2c9344c9e5113a572a14184e34c0f081
mohammadMahdi <mahdi.molaei2000@gmail.com>
2021-03-10 22:22:02 +0330

    my first final project

---
+++ js/btn.js
@@ -0,0 +1,19 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
